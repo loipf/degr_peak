aea5d4837d0f66a9eb75e2c0ced7c4e7560d5070
Carlos Urzua <carlosurzua@FehrmannLab-HP-Z8-G4-Workstation>
2020-12-10 16:20:12 +0100

    correct bug that ignored first column of peak_data this ocurred bcs attach mapping one column was removed to leave only CHR_Mapping gene_id and BP_Mapping and before it also had PROBESET which was removed for generality

--- templates/degr_peak_calling.R
+++ templates/degr_peak_calling.R
@@ -22,13 +22,13 @@ all_mappings_with_source = mappings_with_source_unordered[J(rownames(density_mat
 #### MASTER FOR LOOP to loop through all sources
 clust <- makeCluster(4, type="FORK")
 #### MASTER FOR LOOP to loop through all sources
-regions_list <- parLapply(clust, 5:ncol(all_mappings_with_source), function(source_colnumber)
+regions_list <- parLapply(clust, 4:ncol(all_mappings_with_source), function(source_colnumber)
 {
-  mappings_with_source = all_mappings_with_source[, c(1:4, source_colnumber), with = FALSE]
+  mappings_with_source = all_mappings_with_source[, c(1:3, source_colnumber), with = FALSE]
   
-  source_to_evaluate <- mappings_with_source[[5]] # last column is the source
+  source_to_evaluate <- mappings_with_source[[4]] # last column is the source
   
-  source_name = colnames(mappings_with_source)[5]
+  source_name = colnames(mappings_with_source)[4]
   set_seed = 123456 + source_colnumber
   
   permutations <- cbind(source_to_evaluate, replicate(n_permutations, sample(source_to_evaluate)))
